60b64b273d869afbac34cdb287cc0ae5b8b86b40
Umuril Lyerood <umuril.lyerood@gmail.com>
2024-04-05 01:14:14 +0200

    Add email_is_verified to admin panel

--- clinux/authentication/admin.py
+++ clinux/authentication/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.forms import UserChangeForm
 from django.contrib.auth.admin import UserAdmin
 
 from .models import User
 
-admin.site.register(User, UserAdmin)
+# https://stackoverflow.com/questions/15012235/using-django-auth-useradmin-for-a-custom-user-model
+class MyUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = User
+
+class MyUserAdmin(UserAdmin):
+    form = MyUserChangeForm
+
+    fieldsets = UserAdmin.fieldsets + (
+        ('Custom fields', {
+            'fields': ('email_is_verified', )
+        }),
+    )
+
+admin.site.register(User, MyUserAdmin)
