a3c0511355ebf6e0b40a9506bfae9f4f318984e5
Elia Giacobazzi (graphene) <elia.giacobazzi@gmail.com>
2024-04-07 00:11:13 +0200

    Create a context-processor to handle links

--- clinux/clinux/settings.py
+++ clinux/clinux/settings.py
@@ -70,6 +70,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "pages.context_processor.add_page_links_for_header",
             ],
         },
     },
